9265e957ca977862ac771fc1674546dff06bccf8
Paul-Weisser <paul-weisser@outlook.com>
2019-05-02 12:40:41 +0200

    fixed wrong working dir, causing the scripts getting written into the library

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,12 @@ cd /home/pi/mief
 git clone https://github.com/adafruit/Adafruit_Python_DHT.git && cd Adafruit_Python_DHT
 sudo python3 setup.py install
 
-curl -o apiSensor.py https://raw.githubusercontent.com/Paul-Weisser/mief-is-in-the-script/master/apiSensor.py
-curl -o runMiefSensor.py https://raw.githubusercontent.com/Paul-Weisser/mief-is-in-the-script/master/runMiefSensor.sh 
+cd ..
+
+wget -O apiSensor.py https://raw.githubusercontent.com/Paul-Weisser/mief-is-in-the-script/master/apiSensor.py
+wget -O runMiefSensor.sh https://raw.githubusercontent.com/Paul-Weisser/mief-is-in-the-script/master/runMiefSensor.sh 
+
+chmod +x runMiefSensor.sh
 
 currentcron=$(crontab -l | grep mief)
 
