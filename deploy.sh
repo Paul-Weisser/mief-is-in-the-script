1ae3e0efe0e10ac8d9acaeca58223dddd81ce5ac
Paul-Weisser <paul-weisser@outlook.com>
2019-05-09 17:33:22 +0200

    hurenpi

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,9 @@
 mkdir /home/pi/mief
 cd /home/pi/mief
 
+sudo apt-get install build-essential python-dev python-openssl git
 git clone https://github.com/adafruit/Adafruit_Python_DHT.git && cd Adafruit_Python_DHT
-sudo python3 setup.py install
+sudo python setup.py install
 
 cd ..
 
