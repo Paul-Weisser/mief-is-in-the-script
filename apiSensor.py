f804f103e7d97e57c165c42741d5785ea58d5c2d
Marvin Kaiser <MKaiser1510@gmail.com>
2019-05-09 15:49:09 +0200

    Nicht alles gespeichert :D

--- apiSensor.py
+++ apiSensor.py
@@ -9,6 +9,7 @@ import Adafruit_DHT
 import statistics
 import math
 import logging
+import sys
 from datetime import datetime
 
 
@@ -28,13 +29,20 @@ def main():
 
 	#Verzeichnis für die Config setzen und Config laden
 	config=Config(workingDir + '/apiConf.json')
+
+	#Falls wir commandline Parameter an der Stelle haben übernehmen wir das in die Conifg
+	if len(sys.argv) == 3:
+		piId = sys.argv[1]
+		piSecret = sys.argv[2]
+		config.SetIdAndSecret(piId,piSecret)		
+
 	#Log-Level setzten
 	if not config.debugMode:
 		logger.setLevel(logging.WARNING)
 	#PoolManager für die https requestes erstellen
 	http = urllib3.PoolManager()
 	#Init Sensoren
-	sgp30 = InitSgp30()
+	sgp30 = InitSgp30(config.eCO2Base,config.tVOCBase)
 	dht11 = Adafruit_DHT.DHT11
 	#Listen für den Median erstellen
 	cO2List = []
@@ -66,10 +74,15 @@ def main():
 
 		#Einmal die Minute die Daten an den Server pushen
 		if elapsed_sec % 60 == 0:
-			#Baseline loggen
-			if config.debugMode:
-				eCO2Base, TVOCBase = sgp30.get_iaq_baseline()
+			#Read Baseline
+			eCO2Base, TVOCBase = sgp30.get_iaq_baseline()
+			#Save Baseline
+			try:
+				config.SetBaseLine(eCO2Base,TVOCBase)
+				#Baseline loggen
 				logger.debug("Baseline:\n\teCO2 = 0x%x\n\tTVOC = 0x%x"%(eCO2Base, TVOCBase))
+			except Exception:
+				logger.warning("Could not save Baseline")
 			#Median der Werte berechnen
 			mCo2=statistics.median(cO2List)
 			mHumidity= statistics.median(humidityList)
@@ -84,13 +97,14 @@ def main():
 		time.sleep(1)
 		elapsed_sec += 1
 
-def InitSgp30():
+def InitSgp30(eCO2Base, tVOCBase):
 	try:
 		i2c = busio.I2C(board.SCL, board.SDA, frequency=100000)
 		sgp30 = adafruit_sgp30.Adafruit_SGP30(i2c)
 		logger.debug("SGP30 serial: #" + "".join([hex(i) for i in sgp30.serial]))
 		sgp30.iaq_init()
-		sgp30.set_iaq_baseline(0x8973, 0x8aae)
+		logger.debug("Set Baseline:\n\teCO2 = 0x%x\n\tTVOC = 0x%x"%(eCO2Base, tVOCBase))
+		sgp30.set_iaq_baseline(eCO2Base, tVOCBase)
 		return sgp30
 	except Exception:
 		logger.error("SGP30 init error:", exc_info=True)
@@ -122,9 +136,10 @@ def ConvertRhToAh(humidity, temp):
 
 class Config:
 	def __init__(self, configPath):
-		try:			
+		try:		
+			self.configPath = configPath
 			if not os.path.isfile(configPath):
-				self.CreateConfig(configPath)
+				self.SetConfig(configPath,"-1","-1",4,0,0,"https://mief-is-in-the-air.tk",False,0x8973,0x8aae)
 			self.ReadConfig(configPath)
 		except Exception:
 			#Die Config ist auf jeden Fall da, da wir sie sonst erzeugt hätten
@@ -132,7 +147,7 @@ class Config:
 			#-> Neu erzeugen
 			logger.error("Create config error:", exc_info=True)
 			logger.info("Override old broken config!")
-			self.CreateConfig(configPath)
+			self.SetConfig(configPath,"-1","-1",4,0,0,"https://mief-is-in-the-air.tk",False,0x8973,0x8aae)
 			self.ReadConfig(configPath)
 
 	def ReadConfig(self, path):
@@ -145,18 +160,32 @@ class Config:
 				self.humidityOffset = conf['HumidityOffset']
 				self.apiUrl = conf['ApiUrl']
 				self.debugMode = conf['DebugMode']
-
-	def CreateConfig(self, configPath):
+				self.eCO2Base = conf['ECO2Base']
+				self.tVOCBase = conf['TVOCBase']
+	
+	def SetConfig(self, configPath ,piSecret, piId, dht11pin, tempOff, humidityOff, apiUrl, debugMode, eCO2Base, TVOCBase):
 		defaultConf = {
-				"PiSecret" : "-1",
-				"PiID" : "-1",
-				"Dht11Pin" : 4,
-				"TempOffset" : 0,
-				"HumidityOffset" : 0,
-				"ApiUrl" : "https://mief-is-in-the-air.tk",
-				"DebugMode" : False}
+			"PiSecret" : piSecret,
+			"PiID" : piId,
+			"Dht11Pin" : dht11pin,
+			"TempOffset" : tempOff,
+			"HumidityOffset" : humidityOff,
+			"ApiUrl" : apiUrl,
+			"DebugMode" : debugMode,
+			"ECO2Base": eCO2Base,
+			"TVOCBase" : TVOCBase}
 		with open(configPath, 'w') as conf_out:
 			json.dump(defaultConf, conf_out)
+	
+	def SetBaseLine(self,eCO2Base, tVOCBase):
+		self.SetConfig(self.configPath,self.piSecret,self.piId,self.dht11Pin,self.tempOffset,self.humidityOffset,self.apiUrl,self.debugMode, eCO2Base,tVOCBase)
+
+	def SetIdAndSecret(self,piId, piSecret):
+		try:			
+			self.SetConfig(self.configPath,piSecret,piId,self.dht11Pin,self.tempOffset,self.humidityOffset,self.apiUrl,self.debugMode, self.eCO2Base, self.tVOCBase)
+			self.ReadConfig(self.configPath)
+		except Exception:
+			logger.error("Set PiId or PiSecret faild:", exc_info=True)
 
 if __name__ == "__main__":
 	main()
